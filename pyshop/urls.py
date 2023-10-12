f976e4405ddfe866de53fec10efc8b6021bed524
Asderty Treds <asderty.treds@gmail.com>
2023-10-12 15:00:33 +0300

    add favicons

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('obman/', admin.site.urls),
     path('/', include('products.urls')),
     path('', include('products.urls')),
     path('products/', include('products.urls'))
