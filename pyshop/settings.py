bba41b70891e8b41bb7c0f314ae41bb5ad3652ad
Asderty Treds <asderty.treds@gmail.com>
2023-10-25 09:19:04 +0300

    change db datetime fields

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'grm@_$#@#i$*wm)ct235ln4jg1jxd$^tf&y2wyt5@s%(im)ckr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('SERVERNAMES').split(' ')
 
 
 # Application definition
